111693282b8e650cb6484021f98b9ba74d0b2752
johndavid <91930734+John12david@users.noreply.github.com>
2022-06-14 23:20:29 +0530

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,18 @@
+//Write a java program to print the details of a book
+class Book
+{
+	static String book_name="Rich Dad Poor Dad";
+	static int book_price= 700;
+	static String book_author="Robert Kiyosak";
+	static float book_rating=4.8f;
+	public static void main(String[] args)
+	{
+		System.out.println("Details of Book");
+		System.out.println("************************");
+		System.out.println("Name of the book is "+book_name);
+		System.out.println("Price of the book is "+book_price);
+		System.out.println("Author of the book is "+book_author);
+	}
+
+
+}
