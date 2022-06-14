c8c198662344bf5400410fff24ef7e78d92713e5
johndavid <91930734+John12david@users.noreply.github.com>
2022-06-14 23:21:15 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,16 @@
+class Car
+{
+	static String car_name="Audi";
+	static double bike_price=5000000;
+	static String bike_model="A8";
+	public static void main (String[] args)
+	{
+	    System.out.println("Car Details");
+		System.out.println("**********************");
+		System.out.println("Name of the car is"+car_name);
+		System.out.println("Price of the car is"+car_price);
+		System.out.println("Model of the car is"+car_model);
+
+
+	}
+}
